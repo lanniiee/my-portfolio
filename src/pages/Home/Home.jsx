dd32ea44004bd415cbeea7e295d22d7d8420cd3c
lanniiee <lav250397@hotmail.com>
2022-11-27 19:21:01 +0000

    add animation

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,7 +3,9 @@ import "./Home.scss";
 const Home = () => {
     return (
         <div className="home">
-            <h1>Trainee Software Developer</h1>
+            <h1 className="home__trainee">Trainee</h1>
+            <h1 className="home__software">Software</h1>
+            <h1 className="home__developer">Developer</h1>
         </div>
 
     )
