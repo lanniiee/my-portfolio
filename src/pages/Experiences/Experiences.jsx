17c8c19c0c2b3b3c4c69fe3a4665230451825463
lanniiee <lav250397@hotmail.com>
2022-11-28 15:57:11 +0000

    add experience and skill pages

---
+++ src/pages/Experiences/Experiences.jsx
@@ -0,0 +1,12 @@
+import "./Experience.scss";
+
+const Experiences = () => {
+
+    return (
+        <div className="experiences">
+            <h1 className="experiences__heading"></h1>
+        </div>
+    )
+}
+
+export default Experiences;
---
+++ src/pages/Skills/Skills.jsx
@@ -0,0 +1,12 @@
+import "./Skills.scss";
+
+const Skills = () => {
+    
+    return (
+        <div className="skills">
+            <h1 className="skills__heading">Skills</h1>
+        </div>
+    )
+}
+
+export default Skills;
