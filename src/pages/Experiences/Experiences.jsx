04df52502a6cb2282fb9318d2187175aadbb5fa1
lanniiee <lav250397@hotmail.com>
2022-12-01 18:01:14 +0000

    refactor experience but need to fix extra plus

--- src/pages/Experiences/Experiences.jsx
+++ src/pages/Experiences/Experiences.jsx
@@ -1,64 +1,31 @@
 import "./Experiences.scss";
+import {experiences} from "../../data/experiences";
 
 const Experiences = () => {
+
+    const myEducations = experiences.map(element => {
+        return (
+                    <div className="experiences__info">
+                <h6>{element.date}</h6>
+                <h3 className="experiences__job">{element.title}</h3>
+                <p>
+                    <span className="main-blue">+</span> {element.line1}
+                </p>
+                <p>
+                <span className="main-blue">+</span> {element.line2}
+                </p>
+                <span className="main-blue">+</span> {element.line3}
+                <p>
+                <span className="main-blue">+</span> {element.line4}
+                </p>
+            </div>
+        )
+    })
 
     return (
         <div className="experiences">
             <h1 className="experiences__heading">Experiences</h1>
-
-            <div className="experiences__info">
-                <h6>September 2018 - July 2022</h6>
-                <h3 className="experiences__job">Maths Teacher | OASB, London, UK</h3>
-                <p>
-                    <span className="main-blue">+</span> Teacher of Maths (GCSE & AS Level)
-                </p>
-                <p>
-                <span className="main-blue">+</span> Teacher of Further Maths (GCSE)
-                </p>
-                <span className="main-blue">+</span> Lead House Group (Tutor Group) time
-                <p>
-                <span className="main-blue">+</span> Planned lessons and adapting to class abilities
-                </p>
-            </div>
-
-            <div className="experiences__info">
-                <h6>August 2020 – August 2022</h6>
-                <h3 className="experiences__job">Assistant Manager | Michelle’s Nails, London, UK</h3>
-
-                <p>
-                    <span className="main-blue">+</span> Managing the daily operations of the store
-                </p>
-                <p>
-                    <span className="main-blue">+</span> Assisted manager in supply management and daily and weekly cash management 
-                </p>
-                <p>
-                    <span className="main-blue">+</span> Resolving customer complaint and queries
-                </p>
-            </div>
-
-            <div className="experiences__info">
-                <h6>May 2017 - August 2017</h6>
-                <h3 className="experiences__job">Sales Advisor | JS Sports, London, UK</h3>
-
-                <p>
-                    <span className="main-blue">+</span> Maintain presentation guidelines and basic visual merchandising
-                </p>
-                <p>
-                    <span className="main-blue">+</span> Providing best customer experience and drive performance in sales
-                </p>
-            </div>
-
-            <div className="experiences__info">
-                <h6>September 2014 – September 2015 </h6>
-                <h3 className="experiences__job">Sales Advisor | Superdrug, London</h3>
-
-                <p>
-                    <span className="main-blue">+</span> Developed sales techniques to provide the best customer experience and drive performance in sales
-                </p>
-                <p>
-                    <span className="main-blue">+</span> Awarded with “Best Sale Advisor of The Week” during 1st month of employment
-                </p>
-            </div>
+            {myEducations}
         </div>
     )
 }
