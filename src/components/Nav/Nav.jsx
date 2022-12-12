16d7fc7ac0bc339f6b599844cda365d22bb91ba3
lanniiee <lav250397@hotmail.com>
2022-12-12 15:21:35 +0000

    fix navigation active

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -11,25 +11,30 @@ const Nav = () => {
             <WaterMark />
             <div className="nav__links">
 
-                <NavLink to={"/"}className="nav__links--home" >
+                <NavLink to={"/"} className={({isActive}) => 
+                    isActive ? "nav__links--home-active" : "nav__links--home"} >
                 <h4>Home</h4>
                 </NavLink>
 
-                <NavLink to={"/aboutme"} className="nav__links--aboutme">
+                <NavLink to={"/aboutme"} className={({isActive}) => 
+                    isActive ? "nav__links--aboutme-active" : "nav__links--aboutme"}>
                 <h4>About Me</h4>
                 </NavLink>
 
-                <NavLink to={"/projects"} className="nav__links--projects">
+                <NavLink to={"/projects"} className={({isActive}) => 
+                    isActive ? "nav__links--projects-active" : "nav__links--projects"}>
                 <h4>Projects</h4>
                 </NavLink>
 
 
-                <NavLink to={"/skills"} className="nav__links--skills">
+                <NavLink to={"/skills"} className={({isActive}) => 
+                    isActive ? "nav__links--skills-active" : "nav__links--skills"}>
                 <h4>Skills</h4>
                 </NavLink>
 
 
-                <NavLink to={"/experiences"} className="nav__links--experiences">
+                <NavLink to={"/experiences"} className={({isActive}) => 
+                    isActive ? "nav__links--experiences-active" : "nav__links--experiences"}>
                 <h4>Experiences</h4>
                 </NavLink>
             </div>
