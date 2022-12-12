ddac5885cdebcede9a1a1bafa6646be35f4a8d72
lanniiee <lav250397@hotmail.com>
2022-12-12 13:23:03 +0000

    minor styling fix

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -7,6 +7,7 @@ const Projects = ({projectArr}) => {
     return (
         <div className="projects">
             <h1 className="projects__heading">Projects</h1>
+            <p className="projects__info">Below are some of the projects that I have worked on. Click on the image to visit the page</p>
             <CardList projectArr={projectArr} />
         </div>
     )
